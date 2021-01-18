2d7ed84a3ff1e09350b161e380e8edc2a748d419
Mitesh Chaudhari <mitesh.chaudhari@crestdatasys.com>
2021-01-18 22:38:00 +0530

    with design

--- app.js
+++ app.js
@@ -1,100 +1,237 @@
 const comments = document.querySelector(".comments")
+const nameRegisterContainer = document.querySelector(".name")
+const commentSection = document.querySelector(".comment-section")
 const textbox = document.getElementById("textbox")
 const submitBtn = document.getElementById("submit")
-
+const logoutBtn = document.getElementById("logout")
+const nameRegisterBtn = document.getElementById("nameRegister")
+const nameTextbox = document.getElementById("name")
 let commentsArr = []
+let temp = []
+let username
+// var global
+
+window.addEventListener("load",e=>{
+    
+    firstScreen()
+    
+
+})
+function firstScreen()
+{
+    username=localStorage.getItem("username")
+    if(username)
+    {
+        console.log("name Hai");
+        nameRegisterContainer.classList.add("invisible")
+        nameRegisterContainer.classList.remove("visible")
+
+        commentSection.classList.add("visible")
+        commentSection.classList.remove("invisible")
+
+    }
+    else
+    {
+        console.log("name nahi Hai");
+
+        nameRegisterContainer.classList.add("visible")
+        nameRegisterContainer.classList.remove("invisible")
+        commentSection.classList.add("invisible")
+        commentSection.classList.remove("visible")
+    }
+}
+
 if(localStorage.getItem("comments"))
 {
+    // commentsArr = JSON.parse(localStorage.getItem("comments"))
     commentsArr = JSON.parse(localStorage.getItem("comments"))
-    // console.log(commentsArr);
+    // commentsArr=temp
+    // //console.log(commentsArr);
     renderComments(commentsArr)
 }
+
+nameRegisterBtn.addEventListener("click",e=>{
+    if(nameTextbox.value)
+    {
+        username=nameTextbox.value
+        // commentSection.classList.toggle("visible")
+        // nameRegisterContainer.removeChild(nameRegisterBtn)
+        // nameRegisterContainer.removeChild(nameTextbox)
+        localStorage.setItem("username",nameTextbox.value)
+        firstScreen()
+    }
+})
+
+logoutBtn.addEventListener("click",(e)=>{
+    localStorage.removeItem("username")
+    document.querySelector(".comment-section").classList.toggle("visible")
+    
+    // nameRegisterContainer.appendChild(nameTextbox)
+    // nameRegisterContainer.appendChild(nameRegisterBtn)
+
+    console.log(localStorage.removeItem("username"));
+    username=null
+    firstScreen()
+})
 
 submitBtn.addEventListener("click",e=>{
     e.preventDefault()
 
     console.clear()
-    addComment(true,textbox.value)
+    addComment(true,textbox.value,username)
     localStorage.setItem("comments",JSON.stringify(commentsArr))
-    console.log(JSON.parse(localStorage.getItem("comments")))
+    //console.log(JSON.parse(localStorage.getItem("comments")))
 })
 
 function renderComments(comments)
 {
     comments.forEach(cmt=>{
 
-        console.log(cmt);
-        addComment(false,cmt.text,cmt.parent,cmt.id,cmt.replies)
-        if(cmt.replies.length>0)
+        //console.log(cmt);
+        addComment(false,cmt.text,cmt.name,cmt.likes,cmt.parent,cmt.id,cmt.replies)
+        if(cmt.replies.length > 0)
             renderComments(cmt.replies)
         })
     return
 }
 
-// function render(cmt)
-// {
-//     addComment(cmt.text,cmt.parent,cmt.id,cmt.replies)
-//     if(cmt.replies)
-//         renderComments(cmt)
 
-// }
-
-function addComment(doPush,textboxText,parent,id,replies)
+function addComment(doPush,textboxText,name,likes,parent,id,replies)
 {
-    console.log("from addComment",parent);
-    let obj=commentObj(id,replies,textboxText,parent)
-
+    // console.log(name);
+    let obj=commentObj(id,name,replies,textboxText,likes,parent)
+    // global=obj
     const container = document.createElement("div")
+    const nameTag = document.createElement("p")
+    const likesContainer = document.createElement("span")
+    const repliesContainer = document.createElement("span")
+    const repliesContainer = document.createElement("span")
+    
     container.setAttribute("id",obj.id)
+    
+    nameTag.innerHTML=name
+    nameTag.classList.add("username")
     const commentText = document.createElement("p")
-    const reply = document.createElement("button")
-    
+    commentText.classList.add("comment-text")
     commentText.innerHTML = textboxText
-    reply.innerHTML = "Reply"
-    container.style.border=" 1px solid black"
     
-    reply.addEventListener("click",e=>{
+    const likesIcon = document.createElement("i")
+    likesIcon.innerHTML = likes?likes:0
+    likesIcon.classList.add("fas")
+    likesIcon.classList.add("fa-heart")
+    likesIcon.classList.add("like-icon")
+    // likesIcon.style.display="inline-block"
+    likesContainer.append(likesIcon)
+
+    const repliesIcon = document.createElement("i")
+    repliesIcon.classList.add("far")
+    repliesIcon.classList.add("fa-comment")
+    repliesIcon.classList.add("reply-icon")
+    repliesContainer.append(repliesIcon)
+    
+    // reply.innerHTML = "Reply"
+    container.classList.add("comment")
+    
+    // container.style.border = " 1px solid black"
+    // container.style.padding = "20px"
+    
+    likesIcon.addEventListener("click",(e)=>{
+        //console.log("hi");
+        let parentObj = findParent(commentsArr)
+        function findParent(arr)
+        {
+            let value
+            for(let i=0;i<arr.length;i++)
+            {
+                //console.log("matching:")
+                //console.log(arr[i].id, parent);
+                if(arr[i].id==obj.id)
+                    return arr[i]
+                if(arr[i].replies.length>0)
+                    value=findParent(arr[i].replies)
+                    if(value)
+                        return value
+            }
+        }
+        console.log(parentObj);
+        if(likesIcon.classList.contains("like-icon-liked"))
+            parentObj.likes-=1
+        else
+            parentObj.likes+=1
+        
+        likesIcon.innerHTML=parentObj.likes
+        console.log(this);
+        console.log(parentObj.likes);
+        console.log(commentsArr);
+        localStorage.setItem("comments",JSON.stringify(commentsArr))
+        console.log(localStorage.getItem("comments"));
+        likesIcon.classList.toggle("like-icon-liked")
+    })
+
+    repliesIcon.addEventListener("click",e=>{
         
         const tb = document.createElement("input")
-        const submit = document.createElement("button")
+        const submit = document.createElement("i")
+        const cancel = document.createElement("i")
+        // cancel.innerHTML="&times"
+        cancel.classList.add("far")
+        cancel.classList.add("fa-times-circle")
+        cancel.classList.add("fa-1x")
+        cancel.classList.add("cancel-icon")
+
+        submit.classList.add("far")
+        submit.classList.add("fa-paper-plane")
+        submit.classList.add("send-icon")
         tb.setAttribute("type","text")
-        submit.innerHTML="Reply"
-        container.append(tb,submit)
-        reply.style.display="none"
+        // submit.innerHTML="Reply"
+        container.insertBefore(tb, container.children[3]);
+        container.insertBefore(submit, container.children[4]);
+        container.insertBefore(cancel, container.children[5]);
+        repliesIcon.style.display="none"
 
         submit.addEventListener("click",e=>{
-            addComment(true,tb.value,obj.id)
+            addComment(true,tb.value,username,0,obj.id)
             localStorage.setItem("comments",JSON.stringify(commentsArr))
-            reply.style.display="block"
+            repliesIcon.style.display="inline-block"
             container.removeChild(tb)
             container.removeChild(submit)
-        })  
+            container.removeChild(cancel)
+        })
+        cancel.addEventListener("click",e=>{
+            // addComment(true,tb.value,obj.id)
+            // localStorage.setItem("comments",JSON.stringify(commentsArr))
+            repliesIcon.style.display="inline-block"
+            container.removeChild(tb)
+            container.removeChild(submit)
+            container.removeChild(cancel)
+
+        })
     })
     
-    container.append(commentText,reply)
+    container.append(nameTag,commentText,likesContainer)
     
     // if(doPush)
     // {
         if(parent)
         {
-            console.log("pushing inside",parent);
+            //console.log("pushing inside",parent);
 
             // let parentObj = commentsArr.filter(cmt=>cmt.id==parent)[0]
             let parentObj = findParent(commentsArr)
-            console.log("found?:",parentObj);
+            //console.log("found?:",parentObj);
             function findParent(arr)
             {
                 let value
                 for(let i=0;i<arr.length;i++)
                 {
-                    console.log("matching:")
-                    console.log(arr[i].id, parent);
+                    //console.log("matching:")
+                    //console.log(arr[i].id, parent);
                     if(arr[i].id==parent)
                         return arr[i]
                     if(arr[i].replies.length>0)
                         value=findParent(arr[i].replies)
                         if(value)
-                        return value
+                            return value
                 }
             }
             if(doPush)
@@ -102,7 +239,7 @@ function addComment(doPush,textboxText,parent,id,replies)
             container.style.marginLeft="30px"
             document.getElementById(parentObj.id).append(container)
 
-            console.log("pushed:",obj);
+            //console.log("pushed:",obj);
 
             // parent.replies.push(obj)
             // container.style.marginLeft="30px"
@@ -110,7 +247,7 @@ function addComment(doPush,textboxText,parent,id,replies)
         }
         else
         {
-            console.log(textboxText,"in else");
+            //console.log(textboxText,"in else");
             if(doPush)
                 commentsArr.push(obj)
             comments.append(container)
@@ -118,25 +255,28 @@ function addComment(doPush,textboxText,parent,id,replies)
         // }
         
         // localStorage.getItem("comments")?localStorage.setItem("comments",JSON.stringify([...JSON.parse(localStorage.getItem("comments")),obj])) : localStorage.setItem("comments",JSON.stringify([obj]))
-        
-        console.log(commentsArr);
+        container.append(repliesContainer)
+        //console.log(commentsArr);
         
         
         textbox.value=""
 }
 
-function commentObj(id,rep,comment,parent)
+function commentObj(id,name,rep,comment,liked,parent)
 {
-    if(parent)
-    console.log("returing obj:",parent.id);
+    // if(parent)
+    console.log("liked?",liked);
+
     return{
         id:id ? id : Date.now(),
+        name: name ? name : username,
         parent:parent ? parent : null,
         text:comment,
-        replies:rep ? rep : []
+        replies:rep ? rep : [],
+        likes:liked ? liked : 0
     }
 }
-let x=[{
+let xObj={
     id:Date.now()+"",
     text:"hi",
     replies:[{
@@ -144,4 +284,8 @@ let x=[{
         text:"hi",
         replies:undefined  
     }]
-}]
+}
+
+let x=[
+    xObj
+]
